d196b1aaf38c1e7df5606cf9adb7e46a3454fb7b
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-03 11:48:07 -0300

    form

--- form-web/js/script.js
+++ form-web/js/script.js
@@ -2,6 +2,8 @@
     Criando variáveis
 */
 const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const telefone = document.querySelector("#telefone");
 const button = document.querySelector("button");
 const lista = document.querySelector(".lista");
 
@@ -12,9 +14,13 @@ const lista = document.querySelector(".lista");
     button.addEventListener("click",(event)=>{
         event.preventDefault();
         const inputValue = nome.value;
-        const templateHTML = `<li>${inputValue}</li>`
+        const inputValueEmail = email.value;
+        const inputValueTelefone = telefone.value;
+        const templateHTML = `<li>${inputValue} <br>${inputValueEmail}</br>${inputValueTelefone}</li>`
 
         lista.innerHTML += templateHTML
         nome.value=""
+        email.value=""
+        telefone.value=""
         //incluir itens no HTML
     })
