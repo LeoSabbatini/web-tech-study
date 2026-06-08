d766f98ad5a08517e3ca4c4105907f3a32cd5b68
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-08 10:46:16 -0300

    Update tipodados.js

--- exemplo-js/tipodados.js
+++ exemplo-js/tipodados.js
@@ -3,3 +3,11 @@ let idade = 19;
 
 let casado = false;
 let valorProduto = 42.90;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log("Meu nome e "+nome);
+console.log("Tenho "+idade+" anos");
+ console.log(`R$ ${valorProduto}`);
+ 
+
