94e3c63e6a34858607ef7bf7071aafb00d958c50
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-08 10:34:56 -0300

    add

---
+++ exemplo-js/texto.js
@@ -0,0 +1,11 @@
+let txt = "Estão chegando as provas"
+//tamanho string
+console.log(txt.length) 
+console.log(txt.replace("provas","avaliacoes"));
+console.log(txt.toUpperCase())
+console.log(txt.toLowerCase());
+
+console.log(txt.indexOf("as"));
+console.log(txt.lastIndexOf("as"));
+
+
