ab2a06c0baabacba6d0162ab7477a0e04f25b743
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-08 10:59:18 -0300

    Create calc.js

---
+++ exemplo-js/calc.js
@@ -0,0 +1,10 @@
+let num1 = 10;
+let num2 = 5;
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(div);
