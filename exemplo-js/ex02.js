a1e9c889c21f4f0f05a2f6111bae046e05751691
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-08 11:32:19 -0300

    Update ex02.js

--- exemplo-js/ex02.js
+++ exemplo-js/ex02.js
@@ -1,3 +1,8 @@
-const javaScript = "Java Script"
+/*const javaScript = "Java Script"
 const js = javaScript[0] + javaScript[5]
 console.log(js);
+*/
+
+const mozilla = "Mozilla Developer Network";
+const mdn = mozilla[0] + mozilla[8] + mozilla[18];
+console.log(mdn);
