8fa60298240c1c29e00cc14db097a8ea3c244b9b
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-08 11:50:02 -0300

    Update calcNew.js

--- exemplo-js/calcNew.js
+++ exemplo-js/calcNew.js
@@ -1,6 +1,22 @@
-function meuNome(nome){
+/*function meuNome(nome){
     console.log(nome);
 
 }
+meuNome("Leo da Silva Santos Pinto Barbosa de Oliveira Vidal Martins Neto")*/
 
-meuNome("Leo da Silva Santos Pinto Barbosa de Oliveira Vidal Martins Neto")
+function soma(num1,num2){
+    console.log(num1+num2);
+}
+soma(10,5)
+function sub(num1,num2){
+    console.log(num1-num2);
+}
+sub(10,5)
+function multi(num1,num2){
+    console.log(num1*num2);
+}
+multi(10,5)
+function div(num1,num2){
+    console.log(num1/num2);
+}
+div(10,5)
