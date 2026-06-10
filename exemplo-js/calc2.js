fd97fb63a95a05363746a3a06157111f064fa324
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-10 09:04:32 -0300

    calc2

---
+++ exemplo-js/calc2.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("Digite um numero"))
+let num2 = Number(prompt("Digite outro numero"))
+
+let soma = num1 + num2
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+//console.log(typeof soma)
