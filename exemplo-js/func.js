30aaf64cdfd5e1f1eff8ffdf4848a12ffb47a36b
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-10 08:47:45 -0300

    func

---
+++ exemplo-js/func.js
@@ -0,0 +1,15 @@
+function minhaFuncao(){
+    document.write("OLa");
+}
+
+function pularLinha(){
+    document.write("<br><br")
+}
+
+function mostrarNome(nome){
+    pularLinha()
+    document.write(nome)
+    pularLinha()
+}
+
+mostrarNome("leo")
