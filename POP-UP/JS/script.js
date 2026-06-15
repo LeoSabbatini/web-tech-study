fc4250f53c355ec38d31fa2e9741a188257c36ae
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-15 10:40:51 -0300

    popup exs

---
+++ POP-UP/JS/script.js
@@ -0,0 +1,17 @@
+//alert("pop-up")const button = document.querySelector("button")
+const button = document.querySelector("button")
+const popup = document.querySelector(".popup-wrapper")
+ 
+button.addEventListener("click", function(event) {
+    popup.classList.add("d-block")
+});
+ 
+popup.addEventListener("click", function(event){
+    const classOfClickElement = event.target.classList[0] //Indentifica a classe aonde clica
+    const classNameListArray = ["popup-close", "popup-wrapper"]
+ 
+    if(classNameListArray.includes(classOfClickElement)){
+        popup.classList.remove("d-block")
+    }
+})
+ 
