811d47fa186335475cac21683677d014faeed395
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-08-19 13:49:20 -0300

    feat: fonte e cor text

--- introducao-react/src/components/Anuncio.jsx
+++ introducao-react/src/components/Anuncio.jsx
@@ -8,7 +8,7 @@ const Anuncio = (props) =>{
         <section>
       <article className="anuncio">
         <img src={imgTrompa} className="img-anuncio"/>
-        <h1 className="anuncio-text">{props.title}</h1>
+        <h1>{props.title}</h1>
         <>
         {props.children}
         </>
