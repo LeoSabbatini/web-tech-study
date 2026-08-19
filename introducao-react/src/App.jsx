058620d92be6c90c2f2cdad9ff7005df9e0b8f62
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-08-19 13:22:23 -0300

    feat: link

--- introducao-react/src/App.jsx
+++ introducao-react/src/App.jsx
@@ -26,6 +26,7 @@ function App() {
       <h1>Trompa JUPITER JHR 1100</h1>
       <h2>R$ 16.754,00 BRL</h2>
       <p>Impostos incluídos. Envio calculado no checkout.</p>
+      <a href="https://lamusainstrumentos.es/pt-br/products/trompa-jupiter-jhr-1100?variant=53843448660306&country=BR&currency=BRL&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&srsltid=AfmBOopDwxGCVb0TP9yXt-JS7tmI-g6JiAUObXp5UBDjC_yiJE3AiSiVyLc">Clique aqui para COMPRAR</a>
       </Anuncio>
 
 
