bb270980e92bed47360fb7e0442b0c5cfcb691f0
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-09-02 09:29:39 -0300

    first commit

---
+++ projeto-react/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import "./global.css"
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello, React!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ projeto-react/src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <header className="header">
+        <section className="title-container">
+            <h1 className='title'>Header</h1>
+            <h2>Lorem Ipsum</h2>
+        </section>
+    </header>
+  )
+}
